b2d601ce031e472b5741000b5a8999b4d42700d8
Jacob Mouawad <Jacobmouawad30@gmail.com>
2024-10-28 19:58:05 -0400

    Fixing error about child theme

--- themes/The-Art-of-Golf-Child/functions.php
+++ themes/The-Art-of-Golf-Child/functions.php
@@ -9,6 +9,6 @@
  */
 
  function child_enqueue_scripts(){
-    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css')
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css');
  }
  add_action('wp_enqueue_scripts', 'child_enqueue_scripts');
